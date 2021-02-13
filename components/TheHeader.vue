2cceebdd7bcc2c3690a9819bfe568baccfa2b1c8
tenzyu <tenzyu.on@gmail.com>
2021-02-14 05:43:09 +0900

    allowed more space for header tap action

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -3,12 +3,14 @@
     class="h-16 flex justify-around sticky top-0 left-0 z-50 opacity-75 bg-white border-b shadow"
   >
     <h1 class="my-auto text-lg">
-      <nuxt-link to="/">Mii</nuxt-link>
+      <nuxt-link to="/" class="p-5">Mii</nuxt-link>
     </h1>
-    <nav class="my-auto w-1/2">
+    <nav class="my-auto w-2/3 sm:w-1/2">
       <ul class="flex justify-around text-xs">
         <li v-for="nav in navs" :key="nav.text">
-          <nuxt-link :to="nav.url">{{ nav.text }}</nuxt-link>
+          <nuxt-link :to="nav.url" class="p-5 md:text-md">{{
+            nav.text
+          }}</nuxt-link>
         </li>
       </ul>
     </nav>
