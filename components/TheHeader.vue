8bb59a0a4f67d6156c8b0eadebbe91ce699b3a88
tenzyu <tenzyu.on@gmail.com>
2021-02-18 04:31:05 +0900

    取り急ぎハンバーガーメニューを実装

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,20 +1,52 @@
 <template>
-  <header
-    class="h-16 flex justify-around sticky top-0 left-0 z-50 opacity-75 bg-white border-b shadow"
-  >
-    <h1 class="my-auto text-lg">
-      <nuxt-link to="/" class="p-5">Mii</nuxt-link>
-    </h1>
-    <nav class="my-auto w-2/3 sm:w-1/2">
-      <ul class="flex justify-around text-xs">
-        <li v-for="nav in navs" :key="nav.text">
-          <nuxt-link :to="nav.url" class="py-5 md:text-md">{{
-            nav.text
-          }}</nuxt-link>
+  <div>
+    <header
+      class="h-16 flex justify-around sticky top-0 left-0 z-50 opacity-75 bg-white border-b shadow"
+    >
+      <h1 class="my-auto text-lg">
+        <nuxt-link to="/" class="p-5" v-on:click.native="flag = false"
+          >Mii</nuxt-link
+        >
+      </h1>
+
+      <button
+        v-if="isMobile"
+        id="menu-toggle"
+        class="p-5 focus:outline-none"
+        v-on:click="flag = !flag"
+      >
+        MENU
+      </button>
+      <nav v-else class="my-auto w-2/3 max-w-screen-md">
+        <ul class="flex justify-around text-sm">
+          <li v-for="(nav, index) in navs" :key="index">
+            <nuxt-link :to="nav.url" class="py-5">{{ nav.text }}</nuxt-link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <nav
+      id="menu-body"
+      class="w-full h-screen fixed top-0 left-0 z-30 z-100 h-full bg-gray-700 transition ease-in-out duration-200"
+      :class="{ open: flag }"
+    >
+      <ul class="mx-auto mt-24 space-y-3">
+        <li
+          v-for="(nav, index) in navs"
+          :key="index"
+          class="w-1/2 md:max-w-screen-md mx-auto text-center text-gray-100 text-2xl border-b border-white duration-300"
+        >
+          <nuxt-link
+            class="w-full block py-3"
+            :to="nav.url"
+            v-on:click.native="flag = false"
+          >
+            {{ nav.text }}
+          </nuxt-link>
         </li>
       </ul>
     </nav>
-  </header>
+  </div>
 </template>
 
 <script lang="ts">
@@ -28,11 +60,39 @@ const navs: Nav[] = [
   { url: '/works/', text: 'Works' },
   { url: '/contact/', text: 'Contact' },
   { url: '/history/', text: 'History' },
+  { url: '/brunches/', text: 'Brunches' },
 ]
 
 export default {
   data() {
-    return { navs }
+    return { navs, flag: false, windowWidth: 900 }
+  },
+
+  mounted() {
+    this.windowWidth = window.innerWidth
+    this.$nextTick(() => {
+      window.addEventListener('resize', () => {
+        this.windowWidth = window.innerWidth
+      })
+    })
+  },
+
+  computed: {
+    isMobile(): boolean {
+      // @ts-ignore
+      return this.windowWidth < 1000
+    },
   },
 }
 </script>
+
+<style scoped>
+#menu-body {
+  opacity: 0;
+  transform: translateX(-100%);
+  &.open {
+    opacity: 1;
+    transform: translateX(0%);
+  }
+}
+</style>
