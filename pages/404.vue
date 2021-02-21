14eae5a14d9ece273edadea1788f6ec10d8c5e02
tenzyu <tenzyu.on@gmail.com>
2021-02-21 19:55:49 +0900

    髫縺玲ｩ溯�

--- pages/404.vue
+++ pages/404.vue
@@ -3,22 +3,53 @@
     <main class="container fade">
       <div class="flex mx-auto justify-center items-center">
         <img
-          class="w-4/5 my-4 lg:w-1/2 lg:my-16"
+          class="w-4/5 my-4 lg:w-1/2 lg:my-8"
           src="~/assets/img/page_not_found.svg"
           alt="404 Page を表しているイラスト"
         />
       </div>
-      <h1 class="heading text-center mt-2 lg:text-4xl">
-        ｧｱﾞ～～～:((: ◜◡‾:)):～～～？？！！
-      </h1>
-      <h2 class="heading my-6 text-2xl lg:text-3xl">
-        音速でページを探しましたが見つけられませんでした。
-      </h2>
-      <nuxt-link to="/"
-        ><h3 class="text-blue-700 text-xl lg:text-2xl">
-          Back to the home page.
-        </h3></nuxt-link
-      >
+      <h1 class="heading text-center text-3xl">PAGE NOT FOUND</h1>
+      <div class="container px-4 mx-auto">
+        <hr class="my-8 border" />
+        <div class="text-gray-900 novel">{{ miiNovel.value }}</div>
+        <div class="text-gray-700 border-l border-gray-500 pl-4 my-4 flex">
+          <h2>
+            {{ miiNovel.author }}
+          </h2>
+          <span class="px-2">|</span>
+          <h3>みぃ小説</h3>
+          <span class="pl-2">- {{ miiNovel.identifyText }}</span>
+        </div>
+
+        <nuxt-link to="/"
+          ><h3 class="text-blue-700 text-xl my-4">
+            Back to the home page.
+          </h3></nuxt-link
+        >
+      </div>
     </main>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { miiNovels } from '~/utils/novels'
+
+const miiNovel = miiNovels[Math.floor(Math.random() * miiNovels.length)]
+export default Vue.extend({
+  data() {
+    return { miiNovel }
+  },
+})
+</script>
+
+<style scoped>
+.novel {
+  word-wrap: break-word;
+  -webkit-user-select: text;
+  -moz-user-select: text;
+  -ms-user-select: text;
+  user-select: text;
+  white-space: break-spaces;
+}
+</style>
